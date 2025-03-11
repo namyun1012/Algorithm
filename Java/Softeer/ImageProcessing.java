942d0c5b367a1285f1781d7c09e19c5a89288d44
namyun1012 <96728184+namyun1012@users.noreply.github.com>
2025-03-11 20:04:05 +0900

    03/11

---
+++ Java/Softeer/ImageProcessing.java
@@ -0,0 +1,87 @@
+package Softeer;
+
+import java.io.*;
+import java.util.*;
+
+public class ImageProcessing {
+    static int H, W, Q;
+    static int[] dx = {0,0,-1,1};
+    static int[] dy = {1,-1,0,0};
+    static int[][] board;
+    static int[][] visited;
+
+
+    static class Coord {
+        int x;
+        int y;
+
+        Coord(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        H = Integer.parseInt(st.nextToken());
+        W = Integer.parseInt(st.nextToken());
+
+        board = new int[H+1][W+1];
+
+        for(int i = 1; i <= H; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            for(int j = 1; j <= W; j++) {
+                board[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        Q = Integer.parseInt(br.readLine());
+
+        for (int q = 0; q < Q; q++) {
+            st = new StringTokenizer(br.readLine());
+            int i = Integer.parseInt(st.nextToken());
+            int j = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+            visited = new int[H+1][W+1];
+
+            // BFS 로 탐색하기
+            Queue<Coord> queue = new ArrayDeque<Coord>();
+            int cur_color = board[i][j];
+
+            queue.add(new Coord(i, j));
+            visited[i][j] = 1;
+            board[i][j] = c;
+
+
+            while (!queue.isEmpty()) {
+                Coord cur = queue.remove();
+
+                for (int dir = 0; dir < 4; dir++) {
+                    int nxt_x = cur.x + dx[dir];
+                    int nxt_y = cur.y + dy[dir];
+
+                    if(nxt_x < 1 || nxt_x > H || nxt_y < 1 || nxt_y > W) continue;
+                    if(board[nxt_x][nxt_y] != cur_color) continue;
+                    if(visited[nxt_x][nxt_y] == 1) continue;
+
+                    board[nxt_x][nxt_y] = c;
+                    visited[nxt_x][nxt_y] = 1;
+                    queue.add(new Coord(nxt_x, nxt_y));
+                }
+            }
+        }
+
+        for (int i = 1; i <= H; i++) {
+            for(int j = 1; j <= W; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        br.close();
+    }
+}
---
+++ Java/Softeer/ReserveRoom.java
@@ -0,0 +1,94 @@
+package Softeer;
+import java.io.*;
+import java.util.*;
+
+
+public class ReserveRoom {
+    static int N, M;
+    static Map<String, Integer> map = new HashMap<String, Integer>();
+    static int[][] rooms;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        String[] temp = new String[N];
+        rooms = new int[N][19];
+        // 정렬해서 집어 Map 으로 집어 넣음
+        for(int i = 0; i < N; i++) {
+            temp[i] = br.readLine();
+        }
+        Arrays.sort(temp);
+        
+        for(int i = 0; i < N; i++) {
+            map.put(temp[i], i);
+        }
+
+
+        for(int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            String name = st.nextToken();
+            int start = Integer.parseInt(st.nextToken());
+            int end   = Integer.parseInt(st.nextToken());
+            int index = map.get(name);
+            
+            // rooms[index][9] 는 9 ~ 10 을 의미함
+            for(int j = start; j < end; j++) {
+                rooms[index][j] = 1;
+            }
+        }
+
+        for(int i = 0; i < N; i++) {
+            // 경계 사이에서만 들어가는 코드
+            if (i != 0)
+                System.out.println("-----");
+            // Room 이름
+            System.out.println("Room " + temp[i] + ":");
+
+            // 각 room 마다 찾음
+            List<String> result = new ArrayList<String>();
+
+            for(int j = 9; j <= 17; j++) {
+                // 0일 경우 간격이 어디까지 인지 찾음
+                if(rooms[i][j] == 0) {
+                    int k;
+
+                    for(k = j; k <= 17; k++) {
+                        if(rooms[i][k] == 1)
+                            break;
+                    }
+                    
+                    // j가 10 보다 작을 시 0을 붙여줘야 한다.
+                    StringBuilder sb = new StringBuilder();
+                    
+                    if (j < 10)
+                        sb.append(0);
+
+                    sb.append(j + "-" + k);
+                    result.add(sb.toString());
+                    j = k;
+                }
+            }
+
+            if(result.isEmpty()) {
+                System.out.println("Not available");
+            }
+
+            else {
+                System.out.println(result.size() + " available:");
+                for(String str : result) {
+                    System.out.println(str);
+                }
+            }
+
+        }
+        
+        // 종료 후 br 닫기
+        br.close();
+    }
+
+
+}
